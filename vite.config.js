b340a83039a73d3ef5e6f3db156f26e379dbd6bd
Ekaterina <ekaterinaduzh@gmail.com>
2025-08-15 17:38:40 +0300

    Правка base

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,22 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/fe-diplom_2/',
   server: {
     port: 3000,
-    host: 'localhost',
-    base: '/fe-diplom_2/',
+    host: 'localhost'
   },
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['antd', 'air-datepicker', 'swiper'],
+          utils: ['validator', 'lodash']
+        }
+      }
+    }
   }
 });
