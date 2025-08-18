f851a5cc3555e7812ae0a95e2d46bf07c3a372f9
EkaterinaDuzh <EkaterinaDuzh@users.noreply.github.com>
2025-08-18 11:49:35 +0000

    deploy: 6d71db0bd8ad3d54f672d2eb1ac340a66e64d0a4

---
+++ main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App';
+import './src/index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('No root element found!');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="/fe-diplom_2">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
